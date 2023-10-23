1838c4063bbb0e7d6366b34b61f5efcbfb787284
Kevin Devlin <azwethinkweizkd@gmail.com>
2023-10-22 21:35:36 -0600

    Complete monthly mortgage payment calculation in go and successfully sent it to htmx to display

--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"os"
-	"strings"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gotailwindcss/tailwind/twembed"
@@ -26,57 +27,6 @@ func init() {
     }
 }
 
-func Atoi (s string) int {
-    var (
-        n uint64
-        i int
-        v byte
-    )   
-    for ; i < len(s); i++ {
-        d := s[i]
-        if '0' <= d && d <= '9' {
-            v = d - '0'
-        } else if 'a' <= d && d <= 'z' {
-            v = d - 'a' + 10
-        } else if 'A' <= d && d <= 'Z' {
-            v = d - 'A' + 10
-        } else {
-            n = 0; break        
-        }
-        n *= uint64(10) 
-        n += uint64(v)
-    }
-    return int(n)
-}
-
-
-func Atof(s string) float64 {
-	s = strings.TrimSpace(s) // Remove leading/trailing spaces
-
-    if s == "" {
-        return 0.00
-    }
-
-    var result float64
-    var decimalPlace float64
-
-    for _, ch := range s {
-        if ch == '.' {
-            decimalPlace = 1.0
-        } else if ch < '0' || ch > '9' {
-            return 0.00 // Invalid character in the string
-        } else if decimalPlace == 0 {
-            digit := float64(ch - '0')
-            result = result*10 + digit
-        } else {
-            digit := float64(ch - '0')
-            result = result + digit/decimalPlace
-            decimalPlace *= 10
-        }
-    }
-
-    return result
-}
 func main() {
 	fmt.Println("Mortgage Calulator")
 	dbPassword := os.Getenv("DB_PASSWORD")
@@ -93,16 +43,44 @@ func main() {
 	s := &http.Server{Addr: ":8001", Handler: c.Handler(mux)}
 	
 	h1 := func(w http.ResponseWriter, r *http.Request) {
-		principal := Atof(r.PostFormValue("purchasePrice"))
-		lengthOfMortgageInMonths := Atoi(r.PostFormValue("mortgageTerm"))
-		downPayment := Atof(r.PostFormValue("downPayment"))
-		annualTaxes := Atof(r.PostFormValue("annualTaxes"))
-		interestRate := Atof(r.PostFormValue("interestRate"))
-		annualInsurance := Atof(r.PostFormValue("annualInsurance"))
-		monthlyHoa := Atof(r.PostFormValue("monthlyHoa"))
+		principal := r.PostFormValue("purchasePrice")
+		floatPrincipal, _ := strconv.ParseFloat(principal, 64)  
+		lengthOfMortgageInMonths := r.PostFormValue("mortgageTerm")
+		floatMortgageLength, _ := strconv.ParseFloat(lengthOfMortgageInMonths, 64)  
+		downPayment := r.PostFormValue("downPayment")
+		floatDownPayment, _ := strconv.ParseFloat(downPayment, 64)  
+		annualTaxes := r.PostFormValue("annualTaxes")
+		floatAnnualTaxes, _ := strconv.ParseFloat(annualTaxes, 64)  
+		interestRate := r.PostFormValue("interestRate")
+		floatInterestRate, _ := strconv.ParseFloat(interestRate, 64)  
+		annualInsurance := r.PostFormValue("annualInsurance")
+		floatAnnualInsurance, _ := strconv.ParseFloat(annualInsurance, 64)  
+		monthlyHoa := r.PostFormValue("monthlyHoa")
+		floatMonthlyHoa, _ := strconv.ParseFloat(monthlyHoa, 64)
+		
+		//Maths
+		principalMinusDownPayment := floatPrincipal - floatDownPayment
+		monthlyTaxes := (floatPrincipal/12) * (floatAnnualTaxes/100)
+		monthlyInsurance := floatAnnualInsurance/12
+		monthlyInterestRate := (floatInterestRate/100)/12
+		plusOneMonthlyInterestRate := 1 + monthlyInterestRate
+		exponentialByMortgageLength := math.Pow(plusOneMonthlyInterestRate, floatMortgageLength)
 
+		numerator := monthlyInterestRate * exponentialByMortgageLength
+		denominator := exponentialByMortgageLength - 1
+		division :=(principalMinusDownPayment*numerator)/denominator
 
-		fmt.Printf("Principal: %.2f, Length: %d, Downpayment: %.2f, Annual Taxes: %.2f, Interest Rate: %.2f, Annual Insurance: %.2f, Monthly HOA: %.2f \n", principal, lengthOfMortgageInMonths, downPayment, annualTaxes,interestRate, annualInsurance, monthlyHoa)
+		monthlyMortgagePayment := division + monthlyInsurance + floatMonthlyHoa + monthlyTaxes
+
+		
+response := fmt.Sprintf(`
+<div class="col-span-1">
+	<h3 class="text-white text-6xl text-center m-auto" id="monthlyPayment">
+		$%.2f
+	</h3>
+</div>`, monthlyMortgagePayment)
+
+fmt.Fprint(w, response)
 	}
 
 	mux.HandleFunc("/postMonthlyPayment", h1)
