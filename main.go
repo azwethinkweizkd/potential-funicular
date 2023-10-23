bcd6bd69db1ad3a8b0331cb9ad296ffcd58a847d
Kevin Devlin <azwethinkweizkd@gmail.com>
2023-10-23 16:23:18 -0600

    split post into seperate func and ran through router in main

--- main.go
+++ main.go
@@ -27,6 +27,63 @@ func init() {
     }
 }
 
+func postMonthlyPayment(w http.ResponseWriter, r *http.Request) {
+	principal, _ := strconv.ParseFloat(r.PostFormValue("purchasePrice"), 64) 
+	lengthOfMortgageInMonths, _ := strconv.ParseFloat(r.PostFormValue("mortgageTerm"), 64) 
+	downPayment, _ := strconv.ParseFloat(r.PostFormValue("downPayment"), 64)  
+	annualTaxes, _ := strconv.ParseFloat(r.PostFormValue("annualTaxes"), 64)  
+	interestRate, _ := strconv.ParseFloat(r.PostFormValue("interestRate"), 64) 
+	annualInsurance, _ := strconv.ParseFloat(r.PostFormValue("annualInsurance"), 64) 
+	monthlyHoa,  _ := strconv.ParseFloat(r.PostFormValue("monthlyHoa"), 64) 
+	
+	//Maths
+	principalMinusDownPayment := principal - downPayment
+	monthlyPrincipal := principalMinusDownPayment / lengthOfMortgageInMonths
+	if math.IsNaN(monthlyPrincipal) {
+		monthlyPrincipal = 0
+	}
+	
+	monthlyTaxes := (principal/12) * (annualTaxes/100)
+	monthlyInsurance := annualInsurance/12
+	monthlyInterestRate := (interestRate/100)/12
+	monthlyInterestPayment := monthlyPrincipal * monthlyInterestRate
+	monthlyPrincipalPlusInterest := monthlyPrincipal + monthlyInterestPayment
+	plusOneMonthlyInterestRate := 1 + monthlyInterestRate
+	exponentialByMortgageLength := math.Pow(plusOneMonthlyInterestRate, lengthOfMortgageInMonths)
+
+	numerator := monthlyInterestRate * exponentialByMortgageLength
+	denominator := exponentialByMortgageLength - 1
+	division :=(principalMinusDownPayment*numerator)/denominator
+
+	monthlyMortgagePayment := division + monthlyInsurance + monthlyHoa + monthlyTaxes
+
+	if math.IsNaN(monthlyMortgagePayment) {
+		monthlyMortgagePayment = 0
+	}
+
+	response := fmt.Sprintf(`
+			<h3 class="text-white text-6xl text-center m-auto mb-8" >
+				$%.2f
+			</h3>
+			<div class="flex-grow">
+				<div class="grid grid-cols-2 gap-y-8">
+					<p class="text-xl">Principal & Interest</p>
+					<p class="text-right text-xl">$%.2f</p>
+					<p class="text-xl">Monthly Taxes</p>
+					<p class="text-right text-xl">$%.2f</p>
+					<p class="text-xl">Monthly Insurance</p>
+					<p class="text-right text-xl">$%.2f</p>
+					<p class="text-xl">HOA</p>
+					<p class="text-right text-xl">$%.2f</p>
+					<p class="text-center text-sm italic col-span-2">
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia id aperiam quasi in deleniti voluptate rerum suscipit neque tenetur dignissimos voluptatibus doloremque beatae commodi corrupti, mollitia repudiandae blanditiis perferendis voluptas asperiores pariatur aliquam impedit cumque itaque? Distinctio perspiciatis sed voluptas!
+				  </p>
+				</div>
+			</div>`, monthlyMortgagePayment, monthlyPrincipalPlusInterest, monthlyTaxes, monthlyInsurance, monthlyHoa)
+
+	fmt.Fprint(w, response)
+}
+
 func main() {
 	fmt.Println("Mortgage Calulator")
 	dbPassword := os.Getenv("DB_PASSWORD")
@@ -42,64 +99,7 @@ func main() {
 
 	s := &http.Server{Addr: ":8001", Handler: c.Handler(mux)}
 	
-	h1 := func(w http.ResponseWriter, r *http.Request) {
-		principal, _ := strconv.ParseFloat(r.PostFormValue("purchasePrice"), 64) 
-		lengthOfMortgageInMonths, _ := strconv.ParseFloat(r.PostFormValue("mortgageTerm"), 64) 
-		downPayment, _ := strconv.ParseFloat(r.PostFormValue("downPayment"), 64)  
-		annualTaxes, _ := strconv.ParseFloat(r.PostFormValue("annualTaxes"), 64)  
-		interestRate, _ := strconv.ParseFloat(r.PostFormValue("interestRate"), 64) 
-		annualInsurance, _ := strconv.ParseFloat(r.PostFormValue("annualInsurance"), 64) 
-		monthlyHoa,  _ := strconv.ParseFloat(r.PostFormValue("monthlyHoa"), 64) 
-		
-		//Maths
-		principalMinusDownPayment := principal - downPayment
-		monthlyPrincipal := principalMinusDownPayment / lengthOfMortgageInMonths
-		if math.IsNaN(monthlyPrincipal) {
-    		monthlyPrincipal = 0
-		}
-		
-		monthlyTaxes := (principal/12) * (annualTaxes/100)
-		monthlyInsurance := annualInsurance/12
-		monthlyInterestRate := (interestRate/100)/12
-		monthlyInterestPayment := monthlyPrincipal * monthlyInterestRate
-		monthlyPrincipalPlusInterest := monthlyPrincipal + monthlyInterestPayment
-		plusOneMonthlyInterestRate := 1 + monthlyInterestRate
-		exponentialByMortgageLength := math.Pow(plusOneMonthlyInterestRate, lengthOfMortgageInMonths)
-
-		numerator := monthlyInterestRate * exponentialByMortgageLength
-		denominator := exponentialByMortgageLength - 1
-		division :=(principalMinusDownPayment*numerator)/denominator
-
-		monthlyMortgagePayment := division + monthlyInsurance + monthlyHoa + monthlyTaxes
-
-		if math.IsNaN(monthlyMortgagePayment) {
-    		monthlyMortgagePayment = 0
-		}
-
-		response := fmt.Sprintf(`
-				<h3 class="text-white text-6xl text-center m-auto mb-2" >
-					$%.2f
-				</h3>
-				<div class="flex-grow">
-					<div class="grid grid-cols-2 gap-y-6">
-            			<p class="text-xl">Principal & Interest</p>
-            			<p class="text-right text-xl">$%.2f</p>
-            			<p class="text-xl">Monthly Taxes</p>
-            			<p class="text-right text-xl">$%.2f</p>
-            			<p class="text-xl">Monthly Insurance</p>
-            			<p class="text-right text-xl">$%.2f</p>
-            			<p class="text-xl">HOA</p>
-            			<p class="text-right text-xl">$%.2f</p>
-						<p class="text-center text-sm italic col-span-2">
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia id aperiam quasi in deleniti voluptate rerum suscipit neque tenetur dignissimos voluptatibus doloremque beatae commodi corrupti, mollitia repudiandae blanditiis perferendis voluptas asperiores pariatur aliquam impedit cumque itaque? Distinctio perspiciatis sed voluptas!
-					  </p>
-					</div>
-				</div>`, monthlyMortgagePayment, monthlyPrincipalPlusInterest, monthlyTaxes, monthlyInsurance, monthlyHoa)
-
-		fmt.Fprint(w, response)
-	}
-
-	mux.HandleFunc("/postMonthlyPayment", h1)
+	mux.HandleFunc("/postMonthlyPayment", postMonthlyPayment)
 
     db, err := sql.Open("mysql", fmt.Sprintf("root:%s@tcp(127.0.0.1:3306)/saved_user_mortgagedb", dbPassword))
 	if err != nil {
@@ -114,5 +114,3 @@ func main() {
 	fmt.Println("Now listening on: http://localhost:8001")
 	log.Fatal(s.ListenAndServe())
 }
-
-
