a51da6ca6284b5538d67162698b43acfde93f261
Kevin Devlin <azwethinkweizkd@gmail.com>
2023-10-22 19:12:07 -0600

    Got post route working to begin the calculations for mothly payment, made sure to convert those values to numbers, and some htmx

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gotailwindcss/tailwind/twembed"
@@ -15,10 +15,8 @@ import (
 	"github.com/rs/cors"
 )
 
-type SaveUserMortgage struct {
-	ID        	 int      `json:"id"`
-	principal 	 string   `json:"principal"`
-	mortgageTerm string   `json:"mortgageTerm"`
+type MonthlyPayment struct {
+	MontlyPayment  string
 }
 
 func init() {
@@ -28,6 +26,57 @@ func init() {
     }
 }
 
+func Atoi (s string) int {
+    var (
+        n uint64
+        i int
+        v byte
+    )   
+    for ; i < len(s); i++ {
+        d := s[i]
+        if '0' <= d && d <= '9' {
+            v = d - '0'
+        } else if 'a' <= d && d <= 'z' {
+            v = d - 'a' + 10
+        } else if 'A' <= d && d <= 'Z' {
+            v = d - 'A' + 10
+        } else {
+            n = 0; break        
+        }
+        n *= uint64(10) 
+        n += uint64(v)
+    }
+    return int(n)
+}
+
+
+func Atof(s string) float64 {
+	s = strings.TrimSpace(s) // Remove leading/trailing spaces
+
+    if s == "" {
+        return 0.00
+    }
+
+    var result float64
+    var decimalPlace float64
+
+    for _, ch := range s {
+        if ch == '.' {
+            decimalPlace = 1.0
+        } else if ch < '0' || ch > '9' {
+            return 0.00 // Invalid character in the string
+        } else if decimalPlace == 0 {
+            digit := float64(ch - '0')
+            result = result*10 + digit
+        } else {
+            digit := float64(ch - '0')
+            result = result + digit/decimalPlace
+            decimalPlace *= 10
+        }
+    }
+
+    return result
+}
 func main() {
 	fmt.Println("Mortgage Calulator")
 	dbPassword := os.Getenv("DB_PASSWORD")
@@ -44,27 +93,20 @@ func main() {
 	s := &http.Server{Addr: ":8001", Handler: c.Handler(mux)}
 	
 	h1 := func(w http.ResponseWriter, r *http.Request) {
-		principal := r.PostFormValue("purchasePrice")
-		lengthOfMortgageInMonths := r.PostFormValue("mortgageTerm")
+		principal := Atof(r.PostFormValue("purchasePrice"))
+		lengthOfMortgageInMonths := Atoi(r.PostFormValue("mortgageTerm"))
+		downPayment := Atof(r.PostFormValue("downPayment"))
+		annualTaxes := Atof(r.PostFormValue("annualTaxes"))
+		interestRate := Atof(r.PostFormValue("interestRate"))
+		annualInsurance := Atof(r.PostFormValue("annualInsurance"))
+		monthlyHoa := Atof(r.PostFormValue("monthlyHoa"))
 
-		log.Println(principal, lengthOfMortgageInMonths)
+
+		fmt.Printf("Principal: %.2f, Length: %d, Downpayment: %.2f, Annual Taxes: %.2f, Interest Rate: %.2f, Annual Insurance: %.2f, Monthly HOA: %.2f \n", principal, lengthOfMortgageInMonths, downPayment, annualTaxes,interestRate, annualInsurance, monthlyHoa)
 	}
 
-	h2 := func(w http.ResponseWriter, r *http.Request) { 
-		err := r.ParseForm()
-		if err != nil {
-			http.Error(w, "Failed to parse form", http.StatusBadRequest)
-			return
-		}
-		test := r.PostFormValue("test")
-		log.Println(test)
-		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.Execute(w, test)
-	}
-	http.HandleFunc("/test", h2)
-	http.HandleFunc("/postMonthlyPayment", h1)
+	mux.HandleFunc("/postMonthlyPayment", h1)
 
-	
     db, err := sql.Open("mysql", fmt.Sprintf("root:%s@tcp(127.0.0.1:3306)/saved_user_mortgagedb", dbPassword))
 	if err != nil {
         panic(err.Error())
